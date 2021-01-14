4ca6632651140d84c1723df7237ee20050a43c8c
AyoubHolmes <boulbaz147@gmail.com>
2021-01-14 12:49:39 +0100

    adding js

---
+++ frontend/form/assets/js/dataHandler.js
@@ -0,0 +1,20 @@
+let personalData = {
+    fullname: '',
+    email: '',
+    phone: '',
+    gender: '',
+    birthdate: '',
+    city: '',
+    address: '',
+    passport: '',
+    passport_issue: '',
+    passport_expiry: '',
+    studied: false,
+    studied_city: '',
+    studied_university: '',
+    studied_major: ''
+}
+
+const fullnameHandler = () => {
+    
+}
