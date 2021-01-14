ebb8466b13eab32eb72bd99f6b6bdf4dfadea913
AyoubHolmes <boulbaz147@gmail.com>
2021-01-14 12:57:28 +0100

    testing js 2

--- frontend/form/assets/js/dataHandler.js
+++ frontend/form/assets/js/dataHandler.js
@@ -19,3 +19,28 @@ const fullnameHandler = () => {
     personalData.fullname = document.getElementById("fullname").value;
     console.log(personalData.fullname);
 }
+
+const emailHandler = () => {
+    personalData.email = document.getElementById("email").value;
+    console.log(personalData.email);
+}
+
+const phoneHandler = () => {
+    personalData.phone = document.getElementById("phone").value;
+    console.log(personalData.phone);
+}
+
+const genderHandler = () => {
+    personalData.gender = document.getElementById("gender").value;
+    console.log(personalData.gender);
+}
+
+const birthdateHandler = () => {
+    personalData.birthdate = document.getElementById("birthdate").value;
+    console.log(personalData.birthdate);
+}
+
+const cityHandler = () => {
+    personalData.city = document.getElementById("city").value;
+    console.log(personalData.city);
+}
